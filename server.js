7f21d6d92e0c1da4ff0b17a244ad8c891a55c72c
AntonLev99 <levanovich.anton@gmail.com>
2019-11-09 22:32:54 -0600

    fdfd
    fdf

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.put('/image', image.handleImage(db))
 
 app.post('/imageurl', (req,res) => { image.handleApiCall(req, res)})
 
-app.listen(3100, ()=> {
-	console.log("App is running on port 3100");
+app.listen(process.env.PORT || 3100, ()=> {
+	console.log("App is running on port ${process.env.PORT}");
 })
