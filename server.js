545c0de03114ef9f0564a0581adf6acc44cf7360
AntonLev99 <levanovich.anton@gmail.com>
2019-11-10 17:13:30 -0600

    host

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./Controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-shallow-28430',
     user : 'mac',
     password : '',
     database : 'smart-brain'
